6077bc2a0f8a741984b0ad6b70dac021b8e2df5d
Anthony Rozario <anthony@kraftamine.in>
2024-02-02 18:20:24 +0530

    created a Input component and calling it to contacts

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import TextInput from './textInput';
+
 
 const Contact = () => {
   return (
-    <div className='contact'>contact</div>
+    <div className='contact'>
+       <TextInput>First Name </TextInput>
+       <TextInput>Last Name </TextInput>
+       <TextInput >Phone Number </TextInput>
+       <TextInput>Email </TextInput>
+
+      </div>
   )
 }
 
