200da77655f88d9348891838d539db302131f565
ronimurad <ronimuradwebsite@gmail.com>
2024-02-04 17:37:39 +0530

    new footer update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,46 +3,47 @@ import { SocialIcon } from "react-social-icons"
 const Footer = () => {
   return (
     <>
-   <footer className="footer p-10 bg-slate-50 text-base-content">
+   <footer className="footer p-10 bg-slate-50 text-base-content" style={{justifyContent:"space-between"}} >
   <nav>
     <h6 className="footer-title">Quick Link</h6> 
     <a className="link link-hover">Home</a>
-    <a className="link link-hover">About</a>
-    <a className="link link-hover">Service</a>
+    <a className="link link-hover">About Us</a>
+    <a className="link link-hover">Our Service</a>
+    <a className="link link-hover">Technology</a>
+    <a className="link link-hover">Our Associates</a>
     <a className="link link-hover">Contact Us</a>
   </nav> 
   <nav>
-    <h6 className="footer-title">Our Product</h6> 
-    <a className="link link-hover">LT Electrical Panel</a>
-    <a className="link link-hover">Procection And Control relays</a>
-    <a className="link link-hover">Genset & Spares</a>
-    <a className="link link-hover">THN IT Solutions</a>
+    <h6 className="footer-title">Our Services</h6> 
+    <a className="link link-hover">Web Development</a>
+    <a className="link link-hover">Backend Development</a>
+    <a className="link link-hover">Cloud Service</a>
+    <a className="link link-hover">AI & ML Engineering</a>
   </nav> 
   <nav>
     <h6 className="footer-title">Office Inquiries</h6> 
     <a className="link link-hover">033-2500 5025</a>
-    <a className="link link-hover">info@tsntsolutions.com</a>
+    <a className="link link-hover">info@thnitsolutions.com</a>
     <h6 className="footer-title">Customer Support</h6> 
-    <a className="link link-hover">+91 7044 040 384</a>
-    <a className="link link-hover">+91 9073 322 603</a>
+    <a className="link link-hover">+91 9051959195</a>
+    <a className="link link-hover">+91 9674183723</a>
   </nav>
   <nav>
-    <h6 className="footer-title">Unsatisfied Customer</h6> 
-    <a className="link link-hover">+91 9674 183 723</a>
-    <a className="link link-hover">+91 7450 990 072</a>
-    <a className="link link-hover">nazrul@tsntsolutions.com</a>
+    <h6 className="footer-title">Office Address</h6> 
+    <p className="link link-hover">THN IT Solutions, Rajibpur Road, Gangapur, <br/>
+     Duttapukur-I, Duttapukur- 743248, West Bengal, India
+    </p>
+    <a className="mt-4" href="https://maps.app.goo.gl/Bg24oyCyiZZCgraNA"
+                    target="_blank" rel="noopener noreferrer" style={{textDecoration: "none",  color:"blue", fontSize:"40px"}}>🗺️</a>
   </nav>
 </footer> 
 
 <footer className="footer px-10 py-4 border-t-[2px] bg-slate-50 text-base-content border-base-300">
   <aside className="items-center grid-flow-col">
-    <img src="/logo.jpeg" alt=""  style={{height:"10vh", width:"8vw"}}/>
+    
     <div>
     <h2 className="footer-title font-bold">THN IT Solutiond Pvt Ltd. </h2>
-    <p>We constantly Endeavor our best to<br/>
-       update our technological competence,<br/>
-     introduce new products and achieve<br/> 
-     excellence in our mission.</p>
+  
     </div>
   </aside> 
     <nav className="md:place-self-center md:justify-self-end">
