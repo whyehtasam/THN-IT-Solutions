5bc8399a2a1db0f12a6e501889260d09a8032383
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-02-04 19:31:06 +0530

    Refactor IntroCards component and add new card data

--- src/components/introCards/IntroCards.jsx
+++ src/components/introCards/IntroCards.jsx
@@ -1,17 +1,20 @@
 import Container from "../wrappers/Container";
-import { cardDetails } from "./cardData";
+// import { cardDetails } from "./cardData";
 import lines from "../../assets/cardsLine.svg";
-const IntroCards = () => {
+const IntroCards = (props) => {
+  const data = props.data;
+  console.log(data);
   return (
     <section className="intro-cards h-auto pb-16 px-28 relative">
+        {/* <img src={lines} className="w-full opacity-50 absolute left-0 -top-50  " alt="svg" /> */}
         <img src={lines} className="w-full opacity-50 absolute left-0 bottom-0  " alt="svg" />
-      <Container className="w-full h-full  grid grid-cols-3 gap-8 p-2">
+      <Container className={"w-full h-full justify-items-center grid grid-cols-2 gap-8 p-2 " + props.containerStyle}>
 
-        {cardDetails.map(({id,title,description}) => <div className="card  bg-base-100 shadow-xl hover:scale-105 hover:-translate-y-1 transition-all duration-300" key={id}>
+        {data.map(({id,title,description}) => <div className={"card h-auto bg-base-100 shadow-xl hover:scale-105 hover:-translate-y-1 transition-all duration-300 " + props.cardStyle} key={id}>
           <div className="card-body">
-            <h2 className="card-title ">{title}</h2>
+            <h2 className="card-title text-3xl">{title}</h2>
             <div className="h-[0.5px] bg-opacity-30 bg-black w-10/12 "></div>
-            <p className="text-justify text-sm text-gray-800 ">{description}</p>
+            <p className="text-justify text-xs lg:text-sm text-gray-800 ">{description}</p>
             
           </div>
         </div>)}
