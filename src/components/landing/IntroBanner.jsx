87b24b5ef4cb745d69275f8c4893faf9b59fe2f4
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-02-04 19:29:21 +0530

    Update IntroBanner component to use dynamic title prop

--- src/components/landing/IntroBanner.jsx
+++ src/components/landing/IntroBanner.jsx
@@ -1,11 +1,11 @@
 import Container from "../wrappers/Container"
 import svg from '../../assets/banner.svg'
-const IntroBanner = () => {
+const IntroBanner = (props) => {
   return (
     <section className="banner pb-16 px-28 h-auto">
         <Container className='flex  justify-center items-center w-full h-80 rounded-lg drop-shadow-md border-white   bg-gradient-to-r from-gray-900 via-gray-700 to-black relative overflow-hidden'>
             <img src={svg} className="object-cover w-full absolute " alt="img" />
-        <h1 className="text-5xl w-max text-white font-bold drop-shadow-md animate-pulse">Unveiling THN IT Solutions: A Deep Dive</h1>
+        <h1 className="text-5xl w-max text-white font-bold drop-shadow-md animate-pulse">{props.title}</h1>
         </Container>
     </section>
   )
