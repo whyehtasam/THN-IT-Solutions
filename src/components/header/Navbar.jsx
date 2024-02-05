8a05b3bc7892bb0f15a09386ef78bcd56a75db44
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-02-05 16:39:29 +0530

    Update Navbar styling and background SVG

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -58,8 +58,8 @@ const Navbar = () => {
 
          
           <div className="flex px-2 mx-2"><img src={logo} className="h-16 mix-blend-multiply p-2" alt="" ></img></div>
-          <div className="basis-2/3 hidden md:hidden lg:block">
-            <ul className="menu menu-horizontal flex justify-end">
+          <div className=" hidden md:hidden lg:block">
+            <ul className="menu menu-horizontal  flex justify-end">
               {/* Navbar menu content here */}
               <Link to='/'><li><a className="text-sm font-medium text-gray-800 rounded-sm tracking-wider  hover:font-semibold">Home</a></li> </Link>
               <Link to='/aboutUs'><li><a className="text-sm font-medium text-gray-800 rounded-sm tracking-wider  hover:font-semibold">About Us</a></li> </Link>
