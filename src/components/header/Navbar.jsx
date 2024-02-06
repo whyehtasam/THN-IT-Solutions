e56e71af303b001e8208723bebf3c95275f0778a
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-02-06 13:06:23 +0530

    updated navbar

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -172,13 +172,13 @@ const Navbar = () => {
 };
 
 
-const ServicesMenu = ({ services }) => 
+const ServicesMenu = ({ services },index) => 
 {
   return(
     <div className="flex p-12 mx-[4%]">
       {services.map((service, index) => (
        
-       <div className="">
+       <div className="" key={index}>
 
         <div key={index =="3"} className="border-r-[1px] border-slate-300 px-6 text-left">
           <h3 className="text-lg pb-2">
