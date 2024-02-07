d7068feb03ddec96a65a380dac0a7f13d080b9de
ronimurad <ronimuradwebsite@gmail.com>
2024-02-07 22:35:25 +0530

    slider update and testimonial update

--- src/components/ourService/OurserviceIntro.jsx
+++ src/components/ourService/OurserviceIntro.jsx
@@ -14,7 +14,7 @@ const OurserviceIntro = () => {
         <h1 className="text-3xl  font-bold drop-shadow leading-normal mt-10">
         Revolutionize Your Landscape with THN IT Solutions
         </h1>
-        <p className=" pt-10  text-sm text-justify font-medium leading-loose text-gray-500 w-[500px] ">
+        <p className=" pt-10  text-sm text-justify font-medium leading-loose ">
         Experience the synergy of innovation as THN IT Solutions offers a comprehensive array of services. From custom software development, crafting web and mobile applications, to specialized expertise in web development, we breathe life into your unique business goals. Dive deep into your data with our advanced analytics services, unlocking insights for informed decision-making that propels growth. Elevate your technology infrastructure with our IT consulting services, optimizing efficiency and fortifying security. Ready to initiate transformative change? Connect with THN IT Solutions today.
         </p>
       </div>
---
+++ src/components/portfolio/testimonial.jsx
@@ -0,0 +1,89 @@
+
+function Testimonial(){
+
+return(
+
+   <>
+     <div className="justify-center">
+          <h3 className="text-4xl text-center">What Our Client say</h3>
+        </div>
+        
+        <div className="max-w-[1200px] mx-auto py-[50px] grid lg:grid-cols-4 sm:grid-cols-2 gap-6">
+             <div className="bg-gray-100 shadow-lg p-4">
+              
+                <div className="flex gap-2">
+                <img src="./icin.png" alt="" className="h-[10vh] w-[5vw] rounded-[50%]"/>
+                <div className="justify-start">
+                <h3 className="text-left text-sm font-semibold">Pintu Choudhury  </h3>
+                <p className="text-xs">TSNT Solution</p>
+                <p className="text-xs">Director</p>
+                </div>
+                </div>
+                 <p className="py-4 px-2 text-sm text-justify font-medium  text-gray-500">
+                 TSNT Solutions Pvt. Ltd. is to deliver best quality 
+                 </p>
+                 <div className="rating"></div>
+             </div>
+           
+             <div className="bg-gray-100 shadow-lg p-4">
+              
+                <div className="flex gap-2">
+                <img src="./icin.png" alt="" className="h-[10vh] w-[5vw] rounded-[50%]"/>
+                <div className="justify-start">
+                <h3 className="text-left text-sm font-semibold">Pintu Choudhury  </h3>
+                <p className="text-xs">TSNT Solution</p>
+                <p className="text-xs">Director</p>
+                </div>
+                </div>
+                 <p className="py-4 px-2 text-sm text-justify font-medium  text-gray-500">
+                 TSNT Solutions Pvt. Ltd. is to deliver best quality 
+                 </p>
+                 <div className="rating"></div>
+             </div>
+
+             <div className="bg-gray-100 shadow-lg p-4">
+              
+              <div className="flex gap-2">
+              <img src="./icin.png" alt="" className="h-[10vh] w-[5vw] rounded-[50%]"/>
+              <div className="justify-start">
+              <h3 className="text-left text-sm font-semibold">Pintu Choudhury  </h3>
+              <p className="text-xs">TSNT Solution</p>
+              <p className="text-xs">Director</p>
+              </div>
+              </div>
+               <p className="py-4 px-2 text-sm text-justify font-medium  text-gray-500">
+               TSNT Solutions Pvt. Ltd. is to deliver best quality 
+               </p>
+               <div className="rating"></div>
+           </div>
+        
+           <div className="bg-gray-100 shadow-lg p-4">
+              
+              <div className="flex gap-2">
+              <img src="./icin.png" alt="" className="h-[10vh] w-[5vw] rounded-[50%]"/>
+              <div className="justify-start">
+              <h3 className="text-left text-sm font-semibold">Pintu Choudhury  </h3>
+              <p className="text-xs">TSNT Solution</p>
+              <p className="text-xs">Director</p>
+              </div>
+              </div>
+               <p className="py-4 px-2 text-sm text-justify font-medium  text-gray-500">
+               TSNT Solutions Pvt. Ltd. is to deliver best quality 
+               </p>
+               <div className="rating"></div>
+           </div>
+             
+        </div>
+   </>
+)
+}
+
+
+
+
+
+
+
+
+
+export default Testimonial;
