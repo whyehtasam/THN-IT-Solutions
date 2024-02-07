30886cfc2a188879e3b6c0c6bda4ae7c79a3fe93
Anthony Rozario <anthony@kraftamine.in>
2024-02-07 18:48:17 +0530

    edit nav logo padding

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -167,7 +167,7 @@ const Navbar = () => {
           <div className="container  flex justify-between w-[80%] mx-auto z-5">
 
          
-          <div className="flex px-2 mx-2"><img src={logo} className="h-16 mix-blend-multiply p-2 z-10" alt="" ></img></div>
+          <div className="flex "><img src={logo} className="h-16 mix-blend-multiply py-2 z-10" alt="" ></img></div>
           <div className=" hidden md:hidden lg:block">
             <ul className="menu menu-horizontal p-0 h-[100%]  flex justify-end z-10">
               {/* Navbar menu content here */}
