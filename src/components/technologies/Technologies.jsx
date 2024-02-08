7959005ff5c0277407a6fe44866ddc6492e5b176
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-02-08 17:38:58 +0530

    Update user authentication logic to use bcrypt hashing algorithm

---
+++ src/components/technologies/Technologies.jsx
@@ -0,0 +1,13 @@
+
+import Container from '../wrappers/Container'
+const Technologies = () => {
+  return (
+    <section className='technologies'>
+        <Container>
+            
+        </Container>
+    </section>
+  )
+}
+
+export default Technologies
