11098ceeed6571d726c38c8d38caf9144d67cbed
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-02-09 17:18:06 +0530

    Refactor portfolio component

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,58 +1,51 @@
-
 // import "./global.css"
 //import SimpleSlider from "./slider";
- import Testimonial from "./testimonial1";
-import Portcard from "./portcard"
+// import Testimonial from "./testimonial1";
+// import Portcard from "./portcard";
 
+const Portfolio = () => {
+  return (
+    <>
+      <div className="justify-center ">
+        <h3 className="text-4xl text-center">Our Key Deliveries</h3>
+      </div>
+      <br />
+      <br />
+      <div>
+        <Portcard />
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="justify-center p-20">
+        <h3 className="text-4xl text-center ">What Our Client say</h3>
+      </div>
 
- const Portfolio = () => {
+      <div>
+        <Testimonial />
+      </div>
 
-    return(
-       <>
-         <div className="justify-center ">
-          <h3 className="text-4xl text-center">Our Key Deliveries</h3>
-        </div>
-        <br/>
-        <br/>
-        <div  >
-          <Portcard/>
-          </div>
-           <br/>
-           <br/>
-           <br/>
-           <br/>
-           <br/>
-           <br/>
-           <br/>
-           <br/>
-           <br/>
-           <br/>
-          <div className="justify-center p-20">
-          <h3 className="text-4xl text-center ">What Our Client say</h3>
-        </div>
-    
-          <div >
-          <Testimonial/>
-          
-          </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </>
+  );
+};
 
-           <br/>
-           <br/>
-           <br/>
-           <br/>
-           <br/>
-           <br/>
-           <br/>
-           <br/>
-           <br/>
-           <br/>
-           <br/>
-           <br/>
-          
-        </>
-    )
-
-
- }
-
- export default Portfolio;
+export default Portfolio;
