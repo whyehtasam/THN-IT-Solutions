b7178572eeefd75cbd3fcb0bd9004e1fe03724d0
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-02-12 17:13:35 +0530

    Update user authentication logic to use bcrypt for password hashing

---
+++ src/components/header/DrawerNavbar.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { IntroServiceCardData } from "../ourService/ourServiceData";
+const DrawerNavbar = () => {
+  return (
+    <ul className="gap-4 menu rounded-box">
+      <li className="text-base font-semibold text-gray-800">
+        <Link to="/">Home</Link>
+      </li>
+      <li className="text-base font-semibold text-gray-800">
+        <details>
+          <summary>
+            <Link to="/ourServices">Our Services</Link>
+          </summary>
+          <ul>
+            {IntroServiceCardData.map((service) => (
+              <li key={service.id} className="text-sm text-gray-600 p-1">
+                <Link to={service.linkPath}>
+                  {service.title.split(" ").slice(1).join(" ")}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </details>
+      </li>
+      <li className="text-base font-semibold text-gray-800">
+        <details>
+          <summary>
+            <Link to="/technology">Technology</Link>
+          </summary>
+          <ul>
+            <li className="text-sm text-gray-600 p-2">Frontend Development</li>
+            <li className="text-sm text-gray-600 p-2">Backend Development</li>
+            <li className="text-sm text-gray-600 p-2">Database</li>
+            <li className="text-sm text-gray-600 p-2">Cloud</li>
+            <li className="text-sm text-gray-600 p-2">
+              Artificial Intelligence
+            </li>
+            <li className="text-sm text-gray-600 p-2">
+              Infrastructure Development
+            </li>
+            <li className="text-sm text-gray-600 p-2">DevOps</li>
+          </ul>
+        </details>
+      </li>
+      <li className="text-base font-semibold text-gray-800">
+        <Link to="/portfolio">Portfolio</Link>
+      </li>
+      <li className="text-base font-semibold text-gray-800">
+        <Link to="/career">Career</Link>
+      </li>
+      <li className="text-base font-semibold text-white bg-gray-900 rounded w-fit">
+        <Link to="/contact">Contact</Link>
+      </li>
+    </ul>
+  );
+};
+
+export default DrawerNavbar;
