482c6cb64861b090b97b213112b30eb596925dd1
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-02-13 18:43:06 +0530

    Add new slider images to carousel

--- src/components/carousel/sliderImagePath.js
+++ src/components/carousel/sliderImagePath.js
@@ -1,10 +1,16 @@
-import slider1 from '../../assets/Slider1.jpg';
-import slider2 from '../../assets/Slider2.jpg';
-// import slider3 from '../../assets/slider3.jpg';
-import slider3 from '../../assets/Slider3.png';
+import slider1 from "../../assets/Slider1.jpg";
+import slider2 from "../../assets/Slider2.jpg";
+import slider3 from "../../assets/slider3.jpg";
+import slider4 from "../../assets/slider4.jpg";
+import slider5 from "../../assets/slider5.jpg";
+import slider6 from "../../assets/slider6.jpg";
+// import slider3 from '../../assets/Slider3.png';
 
 export const imagePath = [
-    { id: 1, path: slider1 },
-    { id: 2, path: slider2 },
-    { id: 3, path: slider3 },
+  { id: 1, path: slider1 },
+  { id: 2, path: slider2 },
+  { id: 3, path: slider3 },
+  { id: 4, path: slider4 },
+  { id: 5, path: slider5 },
+  { id: 6, path: slider6 },
 ];
