11a2cafd041e5417518cff6f5e4800e97005f1b8
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-02-15 17:23:28 +0530

    Refactor styling in AboutContainer and WhatWeOffer components

--- src/components/about/AboutContainer.jsx
+++ src/components/about/AboutContainer.jsx
@@ -11,16 +11,18 @@ const AboutContainer = ({ data, style, imgStyle, contentStyle,descStyle }) => {
         <img
           src={path}
           alt="svg"
-          className=" lg:drop-shadow-md  lg:p-0 w-full lg:max-w-3xl"
+          className="w-full  lg:drop-shadow-md lg:p-0 lg:max-w-3xl"
         />
       </div>
       <div
         className={"info flex  flex-col   " + contentStyle}
       >
-        <h1 className="text-2xl font-bold leading-normal lg:text-4xl  md:mt-10">
+        <h1 className="text-2xl font-bold leading-normal lg:text-4xl md:mt-10">
           {title}
         </h1>
-        <p className={"pt-5 mb-5 text-xs font-medium leading-loose text-justify text-gray-500  lg:pt-10 sm:mb-0 lg:text-sm " + descStyle}>
+        <div className="h-[0.5px] bg-opacity-30 bg-black w-10/12 mt-1 md:hidden"></div>
+        {/* <div className="h-[0.5px] bg-opacity-30 bg-black w-5/12 mt-1"></div> */}
+        <p className={"pt-2 md:pt-4 mb-5 text-xs font-medium leading-loose text-justify text-gray-500  lg:pt-6 sm:mb-0 lg:text-sm " + descStyle}>
           {description.split("\n").map((line, i) => (
             <span key={i}>
               {line}
--- src/components/about/WhatWeOffer.jsx
+++ src/components/about/WhatWeOffer.jsx
@@ -11,7 +11,7 @@ const WhatWeOffer = ({ data, style, imgStyle, contentStyle, imgData }) => {
         <div className="grid gap-4 md:my-10  lg:grid-cols-4 md:grid-cols-3">
           {imgData.map(({ tech, path }, index) => (
             <div
-              className="card card-compact flex-1 bg-base-100 border hover:scale-[1.01] lg:max-h-64 max-h-56 lg:max-w-56 p-0 hover:shadow-sm cursor-pointer transition-all duration-200 rounded-md"
+              className="card card-compact flex-1 bg-base-100 md:border md:shadow-none shadow-sm hover:scale-[1.01] lg:max-h-64 max-h-56 lg:max-w-56 p-0 md:hover:shadow-sm cursor-pointer transition-all duration-200 rounded-md"
               key={index}
             >
               <figure className="h-full">
@@ -33,7 +33,8 @@ const WhatWeOffer = ({ data, style, imgStyle, contentStyle, imgData }) => {
         <h1 className="text-2xl font-bold leading-normal lg:text-4xl md:mt-10">
           {title}
         </h1>
-        <p className="pt-5 mb-5 text-xs font-medium leading-loose text-justify text-gray-500 lg:pt-10 sm:mb-0 lg:text-sm">
+        <div className="h-[0.5px] bg-opacity-30 bg-black w-10/12 mt-1 md:hidden"></div>
+        <p className="md:pt-5 pt-2 mb-5 text-xs font-medium leading-loose text-justify text-gray-500 lg:pt-6 sm:mb-0 lg:text-sm">
           {description.split("\n").map((line, i) => (
             <span key={i}>
               {line}
