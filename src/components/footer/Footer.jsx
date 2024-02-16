f31a2dc7a159b706400406fa44c7d485bb0404da
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-02-16 15:32:03 +0530

    Refactor footer component layout and add social media icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -39,24 +39,27 @@ const Footer = () => {
     <a className="opacity-70 text-xs link link-hover">+91 9051959195</a>
     <a className="opacity-70 text-xs link link-hover">+91 9674183723</a>
   </nav>
-  <nav className="w-80 md:w-fit">
-    <div  className="flex justify-between w-full mt-4 md:block">
-    <div className="w-min">
-    <h6 className="footer-title opacity-90">Office Address</h6> 
-    <a className="text-xs opacity-70 link link-hover  tracking-wider">THN IT Solutions, Rajibpur Road, 
-     Gangapur,Duttapukur-I, 
+
+
+  <nav className=" min-w-72 md:max-w-fit ">
+    <div  className="flex flex-1 justify-between w-full  md:block">
+      <div className="w-min">
+         <h6 className="footer-title opacity-90">Office Address</h6> 
+           <a className="text-xs opacity-70 link link-hover  tracking-wider">THN IT Solutions, Rajibpur Road, 
+            Gangapur,Duttapukur-I, 
      Duttapukur- 743248
      West Bengal, India
-    </a>
-    </div>
+           </a>
+      </div>
     <div>
-    <h6 className="mt-2 footer-title opacity-90"></h6>
-    <div className="grid grid-flow-col gap-1">
-            <SocialIcon url="https://www.instagram.com/"  style={{height:"2rem", width:"2rem", borderRadius:"2vw"}} />
+    
+    <div className="flex-1 flex flex-wrap w-full  gap-1">
+
+           <span> <SocialIcon url="https://www.instagram.com/"  style={{height:"2rem", width:"2rem", borderRadius:"2vw"}} /></span>
             {/* <SocialIcon url="https://www.twitter.com/" style={{height:"2rem", width:"2rem", borderRadius:"2vw"}} /> */}
-            <SocialIcon url="https://www.facebook.com/" style={{height:"2rem", width:"2rem", borderRadius:"2vw"}} />
-            <SocialIcon url="https://www.whatsapp.com/" style={{height:"2rem", width:"2rem", borderRadius:"2vw"}} />
-            <SocialIcon url="https://www.linkedin.com/" style={{height:"2rem", width:"2rem", borderRadius:"2vw"}} />
+            <span><SocialIcon url="https://www.facebook.com/" style={{height:"2rem", width:"2rem", borderRadius:"2vw"}} /></span>
+            <span><SocialIcon url="https://www.whatsapp.com/" style={{height:"2rem", width:"2rem", borderRadius:"2vw"}} /></span>
+            <span><SocialIcon url="https://www.linkedin.com/" style={{height:"2rem", width:"2rem", borderRadius:"2vw"}} /></span>
       </div>
       </div>
       </div>
