0364bf735250775776856df9aa81a6fe66c7c916
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-02-19 17:39:02 +0530

    UI update: Made responsive Profile page and Update profile card styles and image paths

--- src/components/portfolio/Profile/LeaderProfilecard.jsx
+++ src/components/portfolio/Profile/LeaderProfilecard.jsx
@@ -1,34 +1,37 @@
-const LeaderProfileCard = ({ leadermsgData }, index) => {
+const LeaderProfileCard = ({ leadermsgData,style}) => {
   return (
-    <>
+    <div className={"md:grid gap-5 flex flex-col" + style}>
       {leadermsgData.map((data, index) => (
-        <div class="mx-auto flex rounded-2xl bg-base-100 drop-shadow-lg border-[1px]">
-          <div className="flex gap-[2%]">
-            <figure className="w-[40%]" key={index}>
-              <div className="rounded-2xl bg-base-100 ">
-                <img
-                  src={data.img}
-                  alt="CEO"
-                  className="h-[40vh] w-full rounded-t-2xl"
-                />
-                <div className="px-[2vw] py-[2vh]">
-                  <h2 className="card-title">{data.Name}</h2>
-                  <p>{data.Designation}</p>
-                </div>
+        // <div className=" gap-5 drop-shadow border-1 md:h-72" key={index}>
+          <div className="flex flex-col  md:flex-row h-full bg-white rounded-2xl p-5 gap-5 md:h-72 shadow-lg " key={index}>
+            <div className=" bg-base-100 aspect-square h-full  flex justify-center items-center ">
+              <img
+                src={data.img}
+                alt="CEO"
+                className="object-cover  h-full rounded-2xl aspect-square"
+              />
+            </div>
+
+            <div className="  grid place-content-between   md:pl-0">
+              <div className="">
+                <h2 className=" text-lg font-semibold py-[2vh]">
+                  {data.MessageTitle}
+                </h2>
+                {data.MsgPara.map((msg, msgIndex) => (
+                  <p className="py-1 text-sm text-justify " key={msgIndex}>
+                    {msg}
+                  </p>
+                ))}
               </div>
-            </figure>
-            <div class=" w-[60%]">
-              <h2 class="font-semibold py-[2vh]">{data.MessageTitle}</h2>
-              {data.MsgPara.map((msg, msgIndex) => (
-                <p className="text-sm px-2 py-1" key={msgIndex}>
-                  {msg}
-                </p>
-              ))}
+              <div className="p-2 pl-0">
+                <h2 className="text-lg font-semibold">{data.Name}</h2>
+                <p className="text-sm font-semibold">{data.Designation}</p>
+              </div>
             </div>
           </div>
-        </div>
+        // </div>
       ))}
-    </>
+    </div>
   );
 };
 
