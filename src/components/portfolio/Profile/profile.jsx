ec7f4425e9784491f5e64439ef76a56c165d1e73
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-02-19 17:42:00 +0530

    Refactor profile component styles

--- src/components/portfolio/Profile/profile.jsx
+++ src/components/portfolio/Profile/profile.jsx
@@ -12,8 +12,8 @@ function Profile() {
   return (
     <section className="mx-auto profile career-page md:mb-8 about-us lg:max-w-7xl">
       <div className="m-5  bg-[url('/Banner.png')] bg-cover rounded-lg md:p-10">
-        <h2 className="p-6 text-2xl font-bold text-center text-white md:text-4xl">
-        Messages from the Top Leaders
+        <h2 className="p-6 text-2xl font-semibold md:font-bold text-center text-white md:text-4xl">
+          Messages from the Top Leaders
         </h2>
       </div>
 
@@ -22,22 +22,25 @@ function Profile() {
       </div>
 
       <div className="flex items-center gap-5 p-5 mx-auto ">
-        <LeaderProfileCard leadermsgData={DirectorTechnicalHead} style=" md:grid-cols-2"/>
+        <LeaderProfileCard
+          leadermsgData={DirectorTechnicalHead}
+          style=" md:grid-cols-2"
+        />
       </div>
       <div className="mx-5 md:mt-10  bg-[url('/Banner.png')] bg-cover rounded-lg md:p-10">
-        <h2 className="p-6 text-4xl font-bold text-center text-white">
-        Technical Expert
+        <h2 className="p-6 text-2xl font-semibold md:font-bold text-center text-white md:text-4xl">
+          Technical Expert
         </h2>
       </div>
       <div className="grid grid-cols-1 gap-5 m-5 md:grid-cols-2 place-content-center">
         <TechnicalExpartCard TechnicalExpartData={TechnicalExpartDatas} />
       </div>
-      <div className="mx-5 md:mt-10  bg-[url('/Banner.png')] bg-cover rounded-lg md:p-10">
-        <h2 className="p-6 text-4xl font-bold text-center text-white">
+      <div className="m-5 md:mt-10  bg-[url('/Banner.png')] bg-cover rounded-lg md:p-10">
+        <h2 className="p-6 text-2xl font-semibold md:font-bold text-center text-white md:text-4xl">
           Technical Team Members
         </h2>
       </div>
-      <div className="grid gap-5 md:gap-10 m-5 md:grid-cols-3 lg:grid-cols-4 place-content-center">
+      <div className="grid gap-5 m-5 md:gap-10 md:grid-cols-3 lg:grid-cols-4 place-content-center">
         <ProfileCards TeamData={TeamDatas} />
       </div>
     </section>
