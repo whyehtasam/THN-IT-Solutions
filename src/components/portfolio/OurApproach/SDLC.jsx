fc0721e392b0522d8485b2028d47ef2684ddd42b
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-02-21 15:30:43 +0530

    Refactor SDLC component styling

--- src/components/portfolio/OurApproach/SDLC.jsx
+++ src/components/portfolio/OurApproach/SDLC.jsx
@@ -21,7 +21,7 @@ function SDLC() {
               
             </ul>
             <li className=" text-sm md:text-[15px] md:font-semibold md:py-[4%] text-slate-600 text-justify">
-              <b className="text-[18px] text-slate-800 text-justify">Planning :</b> In this
+              <b className="sm:text-[18px] text-slate-800 text-justify">Planning:</b> In this
               initial phase, project stakeholders define the scope, objectives,
               requirements, and constraints of the project. This involves
               gathering requirements from clients or users, conducting
@@ -33,21 +33,21 @@ function SDLC() {
         <div className="md:flex grid md:justify-between gap-4 md:mx-[5%] m-2">
           <div className=" md:w-[30%]">
             <li className="text-sm md:text-[15px] md:font-semibold py-[4%] text-justify text-slate-600">
-              <b className="text-[18px] text-slate-800 ">Analysis :</b> During
+              <b className="sm:text-[18px] text-slate-800 ">Analysis:</b> During
               this phase, the requirements gathered in the planning phase are
               analyzed in detail. The goal is to understand the needs of users
               and stakeholders and define the functional and non-functional
               requirements of the software.
             </li>
             <li className="text-sm md:text-[15px] md:font-semibold py-[4%] text-justify text-slate-600">
-              <b className="text-[18px] text-slate-800 ">Design :</b> In the
+              <b className="sm:text-[18px] text-slate-800 ">Design:</b> In the
               design phase, the architecture, structure, and behavior of the
               software are planned and documented. This includes designing the
               user interface, database schema, software components, and system
               architecture.
             </li>
             <li className="text-sm md:text-[15px] md:font-semibold py-[4%] text-justify text-slate-600">
-              <b className="text-[18px] text-slate-800 ">Implementation :</b> In
+              <b className="sm:text-[18px] text-slate-800 ">Implementation:</b> In
               this phase, the actual coding and development of the software take
               place based on the requirements and design specifications.
               Developers write code, create software components, and integrate
@@ -61,7 +61,7 @@ function SDLC() {
 
           <div className=" md:w-[30%]">
             <li className="text-sm md:text-[15px] md:font-semibold py-[4%] text-justify text-slate-600">
-              <b className="text-[18px] text-slate-800 ">Testing :</b> Once the
+              <b className="sm:text-[18px] text-slate-800 ">Testing:</b> Once the
               software is developed, it undergoes rigorous testing to identify
               and fix defects, errors, and bugs. Different types of testing such
               as unit testing, integration testing, system testing, and user
@@ -69,13 +69,13 @@ function SDLC() {
               reliability of the software.
             </li>
             <li className="text-sm md:text-[15px] md:font-semibold py-[4%] text-justify text-slate-600">
-              <b className="text-[18px] text-slate-800 ">Deployment :</b> After
+              <b className="sm:text-[18px] text-slate-800 ">Deployment:</b> After
               successful testing, the software is deployed or released to the
               production environment. This involves installing the software on
               servers, configuring it for use, and making it available to users.
             </li>
             <li className="text-sm md:text-[15px] md:font-semibold py-[4%] text-justify text-slate-600">
-              <b className="text-[18px] text-slate-800 ">Maintenance :</b> The
+              <b className="sm:text-[18px] text-slate-800 ">Maintenance:</b> The
               maintenance phase involves supporting, updating, and enhancing the
               software over its lifecycle. This includes fixing bugs, addressing
               user feedback, adding new features, and adapting the software to
