f218972d034ae11c4c1514c4af0758252563ac54
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-02-24 16:50:05 +0530

    Update ProfileCard styling to include rounded corners

--- src/components/portfolio/Profile/ProfileCard.jsx
+++ src/components/portfolio/Profile/ProfileCard.jsx
@@ -4,7 +4,7 @@ const ProfileCards = ({TeamData}) => {
   return (
     <>
     {TeamData.map((data, index) =>(
-      <div className="sm:mx-auto sm:max-w-96 card card-compact bg-base-100 shadow-lg border" key={index}>
+      <div className="sm:mx-auto sm:max-w-96 card card-compact bg-base-100 shadow-lg border rounded-md md:rounded-lg" key={index}>
         <div className="avatar md:px-10 md:py-6 border-b-[1px] hidden sm:block ">
           <div className="md:w-full w-60 mx-auto rounded-full ring ring-[#397284] ring-offset-base-100 ring-offset-2 my-[2vh] ">
             <img src={data.ImageUrl}  className=""/>
